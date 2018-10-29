c8b4a94e963e9abefe2319d707c369a07b75749c
soumya <soumyachopra@gmail.com>
2018-10-29 19:48:44 +0530

    dapptoken

---
+++ contracts/DappToken.sol
@@ -0,0 +1,13 @@
+prama solidity ^0.4.2;
+
+contract DappToken{
+
+  uint256 public totalSupply;
+  //constructor
+  //Set the total no. of tokens
+  //Read total no. of tokens
+  function DappToken () public{
+    totalSupply =1000000;
+  }
+
+}
