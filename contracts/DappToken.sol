16d1e504b11a0766d0a6791ef53682a581ecacc9
soumya <soumyachopra@gmail.com>
2018-10-30 10:31:31 +0530

    dapptoken

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,13 +1,22 @@
 pragma solidity ^0.4.2;
 
 contract DappToken{
+  //erc20
 
   uint256 public totalSupply;
+
+  mapping( address => uint256) public balanceOf;
+
   //constructor
   //Set the total no. of tokens
-  //Read total no. of tokens
-  function DappToken () public{
-    totalSupply =1000000;
+    function DappToken (uint256 _initialSupply) public{
+
+   //allocate initial supply, here first account is considered admministrator and all tkens assigned to it for testing
+   balanceOf[msg.sender] = _initialSupply;
+    totalSupply = _initialSupply;
+
+
+
   }
 
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var DappToken = artifacts.require("./DappToken.sol");// provides an abstraction of the contract that truffle can use to run in a js environment
 
 module.exports = function(deployer) {
-  deployer.deploy(DappToken);
+  deployer.deploy(DappToken,1000000);//subsequent argument will be passed to constructor
 };
