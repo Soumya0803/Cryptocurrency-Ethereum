931a745984c20eddc8ef62a3813b903b1e17d6bf
soumya <soumyachopra@gmail.com>
2019-01-08 12:25:20 +0530

    tokensaletest

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
 var DappToken = artifacts.require("./DappToken.sol");// provides an abstraction of the contract that truffle can use to run in a js environment
-
+var DappTokenSale=artifacts.require("./DappTokenSale.sol")
 module.exports = function(deployer) {
-  deployer.deploy(DappToken,1000000);//subsequent argument will be passed to constructor
+  deployer.deploy(DappToken, 1000000).then(function() { // subsequent arguments are passed to constructor
+    // Token price is 0.001 Ether
+    var tokenPrice = 1000000000000000;
+    return deployer.deploy(DappTokenSale, DappToken.address, tokenPrice);
+
+});
 };
